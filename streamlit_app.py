928fe5adf9fc5d438fd7408ca41a7977ca0b5717
Matthew Shotts <44091462+MShotts@users.noreply.github.com>
2026-02-11 09:50:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title("Matthew Shotts' Experience")
+st.header("People Supervisor")
+st.write("Matthew has supervised teams from 3 to 11 staff and consultants during his 15 years as a manager.")
+st.write("He successfully promoted team members to the highest levels of their respective job titles and 2 of his former mentees have since been promoted to management.  Additionally, 2 staff followed Matthew to a new company in order to stay under his leadership.")
