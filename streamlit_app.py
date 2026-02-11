5a9e3ac559000cfa132c6e6cc14c4dca1fc40bcb
Matthew Shotts <44091462+MShotts@users.noreply.github.com>
2026-02-11 15:40:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,19 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-# import seaborn as sns
-# import matplotlib.pyplot as plt
 
-st.title("Matthew Shotts' Experience")
-# st.header("People Supervisor")
-# st.write("Matthew has supervised teams from 3 to 11 staff and consultants during his 15 years as a manager.")
-# st.write("He successfully promoted team members to the highest levels of their respective job titles and 2 of his former mentees have since been promoted to management.  Additionally, 2 staff followed Matthew to a new company in order to stay under his leadership.")
+st.sidebar.header("TBD")
+
+st.markdown("""
+    <style>
+    .stApp {
+        background: linear-gradient(to bottom, #336699, #2F4858);
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown('<span style="font-size:48px; color:#9EE493">**Matthew Shotts**</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:22px; color:#DAF7DC">Delivering Insights | Improving Processes | Empowering Staff</span>', unsafe_allow_html=True)
 
 df_years_exp=pd.DataFrame({
     "Domain": ["Project Mgmt","People Mgmt","SAS/SQL","DataViz/PBI","Cloud DW","Agile"],
@@ -16,15 +22,16 @@ df_years_exp=pd.DataFrame({
 })
 
 years_exp_chart = alt.Chart(df_years_exp).mark_bar().encode(
-    x=alt.X('Years_Experience',axis=alt.Axis(tickCount=5)),
+    x=alt.X('Years_Experience',title='Years of Experience',axis=alt.Axis(tickCount=5)),
     y=alt.Y('Domain',sort='-x',title=None)
+).configure(
+    background='transparent'
+).properties(
+    # width='container',
+    width=510,
+    height=250
 )
-st.altair_chart(years_exp_chart, use_container_width=True)
-
-# fig, ax = plt.subplots(figsize=(10, 6))
-# sns.barplot(data=df_years_exp, x='Domain', y='Years_Experience', ax=ax, orient='h')
-# # ax.set_title('Monthly Sales (Horizontal)')
-# st.pyplot(fig)
+st.altair_chart(years_exp_chart, use_container_width=False)
 
 st.subheader("Drove enterprise-level analytics strategy and execution across healthcare, retail, and assessment organizations:")
 st.text("Delivering cost savings (six-figure annual vendor fee reductions), improved decision-making, and scalable data products used by hundreds of users weekly.")
