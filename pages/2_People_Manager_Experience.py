85be860d762614459fb6c0f638efec50ae549918
Matthew Shotts <44091462+MShotts@users.noreply.github.com>
2026-02-12 10:30:56 -0500

    Update 2_People_Manager_Experience.py

--- pages/2_People_Manager_Experience.py
+++ pages/2_People_Manager_Experience.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
-import pandas as pd
-import altair as alt
+# import pandas as pd
+# import altair as alt
 
 # st.sidebar.header("TBD")
 
@@ -13,12 +13,54 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.markdown('<span style="font-size:48px; color:#9EE493">**People Manager Experience**</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:48px; color:#9EE493">**People Manager**</span>', unsafe_allow_html=True)
 # st.markdown('<span style="font-size:22px; color:#DAF7DC">People Manager Experience</span>', unsafe_allow_html=True)
+st.write("")
+st.write("")
 
-st.markdown('<span style="font-size:22px; color:#DAF7DC">Empowering staff</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Matthew has supervised as many as 12 staff.  This team was comprised of 9 direct reports and 3 consultants.</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">During his tenure as manager, Matthew has promoted many team members to higher job titles and increased levels of responsibility.</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">Two staff previously mentored by Matthew have been promoted to managerial positions.</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:14px; color:#FFFFFF">His teams had excellent retention with only one team member leaving for another company.  Additionally, two team members actually followed Matthew from ETS to Burlington Stores </span>', unsafe_allow_html=True)
+pexp_col1, pexp_col2, pexp_col3 = st.columns(3)
 
+with pexp_col1:
+    st.markdown('''
+        <div style="text-align: center;">
+            <p style="color: #FFFFFF; font-size: 18px; margin-bottom: 0px;">
+                Years Experience
+            </p>
+            <h1 style="color: #DAF7DC; font-size: 42px; margin: 0;">
+                15
+            </h1>
+        </div>
+        ''', unsafe_allow_html=True)
+
+with pexp_col2:
+    st.markdown('''
+        <div style="text-align: center;">
+            <p style="color: #FFFFFF; font-size: 18px; margin-bottom: 0px;">
+                Max Team Size
+            </p>
+            <h1 style="color: #DAF7DC; font-size: 42px; margin: 0;">
+                12
+            </h1>
+        </div>
+        ''', unsafe_allow_html=True)
+
+with pexp_col3:
+    st.markdown('''
+        <div style="text-align: center;">
+            <p style="color: #FFFFFF; font-size: 18px; margin-bottom: 0px;">
+                Staff Hired
+            </p>
+            <h1 style="color: #DAF7DC; font-size: 42px; margin: 0;">
+                ~15
+            </h1>
+        </div>
+        ''', unsafe_allow_html=True)
+
+st.write("")
+# st.markdown('<span style="font-size:22px; color:#DAF7DC">Empowering staff</span>', unsafe_allow_html=True)
+# st.markdown('<span style="font-size:14px; color:#FFFFFF">Matthew has supervised as many as 12 staff.  This team was comprised of 9 direct reports and 3 consultants.</span>', unsafe_allow_html=True)
+# st.markdown('<span style="font-size:14px; color:#FFFFFF">Matthew has had the privilege of building multiple teams and personally hired ~15 staff.</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:22px; color:#DAF7DC">Highlights:</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:14px; color:#FFFFFF">During his tenure as manager, Matthew has promoted many team members to higher job titles and increased levels of responsibility; including one team member promoted to the highest level of a 4-tier job title.</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:14px; color:#FFFFFF">Members of the team were recognized each year as high performers during performance calibrations across the department.  Two staff previously mentored by Matthew have been promoted to managerial positions.</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:14px; color:#FFFFFF">His teams had excellent retention with only one team member leaving for another company.  Additionally, two team members followed Matthew from ETS to Burlington Stores to stay under his leadership and participate in the Merch 2.0 reporting initiative.</span>', unsafe_allow_html=True)
