469c26e6fa5cb0d6976197b8d28a2aa114f517d0
Matthew Shotts <44091462+MShotts@users.noreply.github.com>
2026-02-12 09:41:10 -0500

    Update 2_People_Manager_Experience.py

--- pages/2_People_Manager_Experience.py
+++ pages/2_People_Manager_Experience.py
@@ -13,11 +13,12 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.markdown('<span style="font-size:48px; color:#9EE493">**Matthew Shotts**</span>', unsafe_allow_html=True)
-st.markdown('<span style="font-size:22px; color:#DAF7DC">People Manager Experience</span>', unsafe_allow_html=True)
+st.markdown('<span style="font-size:48px; color:#9EE493">**People Manager Experience**</span>', unsafe_allow_html=True)
+# st.markdown('<span style="font-size:22px; color:#DAF7DC">People Manager Experience</span>', unsafe_allow_html=True)
 
 st.markdown('<span style="font-size:22px; color:#DAF7DC">Empowering staff</span>', unsafe_allow_html=True)
 st.markdown('<span style="font-size:14px; color:#FFFFFF">Matthew has supervised as many as 12 staff.  This team was comprised of 9 direct reports and 3 consultants.</span>', unsafe_allow_html=True)
 st.markdown('<span style="font-size:14px; color:#FFFFFF">During his tenure as manager, Matthew has promoted many team members to higher job titles and increased levels of responsibility.</span>', unsafe_allow_html=True)
 st.markdown('<span style="font-size:14px; color:#FFFFFF">Two staff previously mentored by Matthew have been promoted to managerial positions.</span>', unsafe_allow_html=True)
 st.markdown('<span style="font-size:14px; color:#FFFFFF">His teams had excellent retention with only one team member leaving for another company.  Additionally, two team members actually followed Matthew from ETS to Burlington Stores </span>', unsafe_allow_html=True)
+
